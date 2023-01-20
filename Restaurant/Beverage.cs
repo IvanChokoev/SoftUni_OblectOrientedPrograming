6d0a2f9b76846316e0910d18be760328384b1ff7
Ivan Chokoev <102393638+IvanChokoev@users.noreply.github.com>
2023-01-20 23:06:30 +0200

    Add files via upload

---
+++ Restaurant/Beverage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Beverage : Product
+    {
+        public virtual double Milliliers { get; set; }
+        public Beverage(string name, decimal price, double milliliters) 
+            : base(name, price)
+        {
+            this.Milliliers = milliliters;
+        }
+
+    }
+}
