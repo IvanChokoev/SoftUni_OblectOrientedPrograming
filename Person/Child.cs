43b356396a98dfad60c1441acf02fc8a9c4797ed
Ivan Chokoev <102393638+IvanChokoev@users.noreply.github.com>
2023-01-20 19:50:37 +0200

    Add files via upload

---
+++ Person/Child.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string aName, int aAge)
+            : base(aName, aAge)
+        {
+
+        }
+        public override int Age
+        {
+            get
+            {
+                return base.Age;
+            }
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Age cannot be graer than 15"); 
+                }
+                base.Age = value;
+            }
+        }
+    }
+}
