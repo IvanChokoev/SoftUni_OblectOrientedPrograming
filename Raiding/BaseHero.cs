cf84d66fea875d2ddb9d5f499643bde2191c57be
Ivan Chokoev <102393638+IvanChokoev@users.noreply.github.com>
2023-01-23 18:09:38 +0200

    Add files via upload

---
+++ Raiding/BaseHero.cs
@@ -0,0 +1,15 @@
+﻿namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        public BaseHero(string name, int power)
+        {
+            this.Power = power;
+            this.Name = name;
+        }
+        public string Name { get; }
+        public int Power { get; }
+        public abstract string CastAbility();
+     
+    }
+}
